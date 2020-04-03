e63627a7002c2d9ef807a781f22995e48d7f65c4
Guilherme Veloso <velosologan@gmail.com>
2020-04-03 20:49:59 -0300

    Testando...

--- src/br/upis/ClientOperation.java
+++ src/br/upis/ClientOperation.java
@@ -15,7 +15,7 @@ public class ClientOperation {
 		throws MalformedURLException, RemoteException, NotBoundException {
 		
 		reference = (RMIInterface) Naming.lookup("//localhost/MyServer");
-		String txt = JOptionPane.showInputDialog("What is your name?");
+		String txt = JOptionPane.showInputDialog("Qual o seu nome?");
 			
 		String response = reference.helloTo(txt);
 		JOptionPane.showMessageDialog(null, response);
